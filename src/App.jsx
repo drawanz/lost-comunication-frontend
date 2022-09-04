1831b2dc3a2355e3447076216d469518e6055da9
Leonidas <leonidas.drawanz@gmail.com>
2022-09-04 11:17:49 -0300

    chore: adicionando app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Container from 'react-bootstrap/Container';
+import HomePage from './pages/homePage/homePage';
+
+function App() {
+  return (
+    <Container className="App">
+      <HomePage />
+    </Container>
+  );
+}
+
+export default App;
