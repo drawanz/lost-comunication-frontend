48935f4d9589bd43d44af21da6d1156f8f91f082
Leonidas <leonidas.drawanz@gmail.com>
2022-09-05 13:37:23 -0300

    feat: editando opções na homePage

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -8,15 +8,18 @@ export default function HomePage() {
       <h4>Bem vindo à interface de comunicação de perda</h4>
 
       <div>
-        <p>Para consultar, atualizar ou excluir uma perda, por favor, consulte o CPF no campo abaixo</p>
-        <input />
-        <Button>Consultar</Button>
+        <p>Para inserir uma nova perda, por favor, clique no botão abaixo</p>
+        <Button>Quero inserir uma perda</Button>
       </div>
 
+      <div>
+        <p>Para consultar ou atualizar uma perda, por favor, clique no botão abaixo</p>
+        <Button>Quero consultar uma perda</Button>
+      </div>
 
       <div>
-        <p>Para inserir uma nova perda, por favor, clique no botão abaixo</p>
-        <Button>Inserir uma nova perda</Button>
+        <p>Para excluir uma perda, por favor, clique no botão abaixo</p>
+        <Button>Quero excluir uma perda</Button>
       </div>
 
     </Container>
