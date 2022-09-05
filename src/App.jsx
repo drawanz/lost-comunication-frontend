f7f01169fbbab0a89ed82e4ab88669cb47be48f8
Leonidas <leonidas.drawanz@gmail.com>
2022-09-05 13:38:04 -0300

    feat: adicionando rota /

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Container from 'react-bootstrap/Container';
-import GeneralForm from './components/generalForm/generalForm';
-// import HomePage from './pages/homePage/homePage';
+import HomePage from './pages/homePage/homePage';
 
 function App() {
   return (
     <Container className="App">
-      {/* <HomePage /> */}
-      <GeneralForm />
+      <HomePage />
     </Container>
   );
 }
