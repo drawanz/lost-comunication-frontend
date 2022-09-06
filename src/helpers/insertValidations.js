d688df8e79269ed9c87ce187c45f493eee4e5eaf
Leonidas <leonidas.drawanz@gmail.com>
2022-09-06 11:11:03 -0300

    feat: validações para inserir no db

---
+++ src/helpers/insertValidations.js
@@ -0,0 +1,78 @@
+import { validate } from 'gerador-validador-cpf';
+
+const verifyName = (name) => {
+  if (name === '') {
+    return { message: 'Nome não pode ser vazio' };
+  }
+  if (name.length < 8) {
+    return { message: 'Nome precisa ter mais de 8 caracteres' };
+  }
+  return true;
+};
+
+const verifyEmail = (email) => {
+  if (!email.match(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/)) {
+    return { message: 'Email precisa ser válido' };
+  }
+  return true;
+};
+
+const verifyCpf = (cpf) => {
+  if (!validate(cpf)) {
+    return { message: 'CPF inválido' };
+  }
+  return true;
+};
+
+const verifyCoords = (latitude, longitude) => {
+  if (latitude < -90 || latitude > 90) {
+    return { message: 'Latitude precisa estar entre -90° e 90°' };
+  }
+  if (longitude < -180 || longitude > 180) {
+    return { message: 'Longitude precisa estar entre -180° e 180°' };
+  }
+  return true
+};
+
+const verifyCrop = (typeCrop) => {
+  if (typeCrop === '') {
+    return { message: 'Lavoura não pode ser vazio' };
+  }
+  return true;
+};
+
+const verifyDate = (date) => {
+  if (
+    !date.match(/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/)
+  ) {
+    return { message: 'Data precisa estar no formado dia/mês/ano' };
+  }
+  return true;
+};
+
+const verifyEvent = (event) => {
+  if (event === '') {
+    return { message: 'Evento não pode ser vazio' };
+  }
+  return true;
+};
+
+export default function insertValidations(payload) {
+  const verifyingName = verifyName(payload.name);
+  const verifyingEmail = verifyEmail(payload.email);
+  const verifyingCpf = verifyCpf(payload.cpf);
+  const verifyingCoords = verifyCoords(payload.latitude, payload.longitude);
+  const verifyingCrop = verifyCrop(payload.typeCrop);
+  const verifyingDate = verifyDate(payload.date);
+  const verifyingEvent = verifyEvent(payload.event);
+
+  if (verifyingName.message) return verifyingName.message;
+  if (verifyingEmail.message) return verifyingEmail.message;
+  if (verifyingCpf.message) return verifyingCpf.message;
+  if (verifyingCoords.message) return verifyingCoords.message;
+  if (verifyingCrop.message) return verifyingCrop.message;
+  if (verifyingDate.message) return verifyingDate.message;
+  if (verifyingEvent.message) return verifyingEvent.message;
+
+  return true;
+}
