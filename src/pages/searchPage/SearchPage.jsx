ecb8057aae18fdd1cc3be0cd44f1a6649124d8c9
Leonidas <leonidas.drawanz@gmail.com>
2022-09-06 19:23:26 -0300

    feat: removendo opção de delete da homepage

--- src/pages/searchPage/SearchPage.jsx
+++ src/pages/searchPage/SearchPage.jsx
@@ -1,54 +1,103 @@
 import Container from 'react-bootstrap/Container';
-// import GeneralForm from '../../components/generalForm/GeneralForm';
 import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
 import Axios from 'axios';
-import { Button } from 'react-bootstrap';
-import { validate } from 'gerador-validador-cpf';
+import { Button, Spinner } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import cpfValidation from '../../helpers/cpfValidation.js';
+import {
+  changeCpf,
+  changeData,
+  changeEmail,
+  changeEvento,
+  changeLatitude,
+  changeLavoura,
+  changeLongitude,
+  changeName,
+} from '../../redux/reducers/lostCommunicationSlice.js';
+import ShowLostComponent from '../../components/showLostComponent/ShowLostComponent.jsx';
 
-
-export default function SearchPage() {
+export default function SearchComponent() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [cpf, setCpf] = useState('');
-  const [lost, setLost] = useState('');
+  const [searchCompleted, setSearchCompleted] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [deleteMode, setDeleteMode] = useState(false);
 
   const handleSearch = async () => {
-    const cpfIsValid = validate(cpf);
+    const cpfIsValid = cpfValidation(cpf);
     if (!cpfIsValid) {
       alert('CPF inválido');
     }
-    try {
-      const {
-        data: { data },
-      } = await Axios.get(`https://softfocus-lc-api.herokuapp.com/lost/${cpf}`);
-      setLost(data);
-    } catch (e) {
-      alert(e.response.data.detail);
+    if (cpfIsValid) {
+      try {
+        setLoading(true);
+        const {
+          data: { data },
+        } = await Axios.get(
+          `https://softfocus-lc-api.herokuapp.com/lost/${cpf}`
+        );
+        dispatch(changeData(data[0].data_colheita));
+        dispatch(changeEmail(data[0].email));
+        dispatch(changeEvento(data[0].evento));
+        dispatch(changeLatitude(data[0].latitude));
+        dispatch(changeLavoura(data[0].lavoura));
+        dispatch(changeLongitude(data[0].longitude));
+        dispatch(changeName(data[0].nome));
+        dispatch(changeCpf(data[0].CPF));
+        setSearchCompleted(true);
+        setLoading(false);
+      } catch (e) {
+        alert(e.response.data.detail);
+      }
     }
   };
 
   return (
     <Container>
-      <Form>
-        <Form.Group className="mb-3" controlId="cpfFormGroup">
-          <Form.Label>CPF do produtor rural</Form.Label>
-          <Form.Control
-            type="number"
-            onChange={(e) => setCpf(e.target.value)}
-            value={cpf}
-            placeholder="Use apenas números"
-          />
-        </Form.Group>
-      </Form>
-
-      <Button onClick={() => handleSearch()}>Buscar</Button>
+      {searchCompleted ? (
+        <>
+          <ShowLostComponent />
+          {deleteMode ? (
+            <div>delete mode on</div>
+          ) : (
+            <>
+              <Button onClick={() => navigate('/update')}>Editar</Button>
+              <Button onClick={() => setDeleteMode(true)}>Deletar</Button>
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          <Form>
+            <Form.Group className="mb-3" controlId="cpfFormGroup">
+              <Form.Label>CPF do produtor rural</Form.Label>
+              <Form.Control
+                type="number"
+                onChange={(e) => setCpf(e.target.value)}
+                value={cpf}
+                placeholder="Use apenas números"
+              />
+            </Form.Group>
+          </Form>
+          {loading ? (
+            <Button variant="primary" disabled>
+              <Spinner
+                as="span"
+                animation="border"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              Loading...
+            </Button>
+          ) : (
+            <Button onClick={() => handleSearch()}>Buscar</Button>
+          )}
+        </>
+      )}
     </Container>
   );
 }
-// export default function InsertPage() {
-//   return (
-//     <Container>
-//       <SearchComponent />
-//       <GeneralForm />
-//     </Container>
-//   );
-// }
