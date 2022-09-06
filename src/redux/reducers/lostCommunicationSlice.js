adb926b894c8822060590834278be5cd1aa36023
Leonidas <leonidas.drawanz@gmail.com>
2022-09-06 16:57:33 -0300

    feat: criando reducers

--- src/redux/reducers/lostCommunicationSlice.js
+++ src/redux/reducers/lostCommunicationSlice.js
@@ -1,17 +1,54 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [];
+const initialState = {
+  isLoading: false,
+  nome: '',
+  email: '',
+  CPF: '',
+  latitude: '',
+  longitude: '',
+  lavoura: '',
+  data_colheita: '',
+  evento: '',
+};
 
 export const lostCommunicationSlice = createSlice({
   name: 'lostCommunication',
   initialState,
   reducers: {
-    add: (state, action) => {
-      state.value.push(action);
-    },
+    changeName: (state, { payload }) => ({ ...state, nome: payload }),
+    changeEmail: (state, { payload }) => ({ ...state, email: payload }),
+    changeCpf: (state, { payload }) => ({ ...state, CPF: payload }),
+    changeLatitude: (state, { payload }) => ({ ...state, latitude: payload }),
+    changeLongitude: (state, { payload }) => ({ ...state, longitude: payload }),
+    changeLavoura: (state, { payload }) => ({ ...state, lavoura: payload }),
+    changeData: (state, { payload }) => ({ ...state, data_colheita: payload }),
+    changeEvento: (state, { payload }) => ({ ...state, evento: payload }),
   },
+  clean: (state) => ({
+    ...state,
+    isLoading: false,
+    nome: '',
+    email: '',
+    CPF: '',
+    latitude: '',
+    longitude: '',
+    lavoura: '',
+    data_colheita: '',
+    evento: '',
+  }),
 });
 
-export const { add } = lostCommunicationSlice.actions;
+export const {
+  changeName,
+  changeEmail,
+  changeCpf,
+  changeLatitude,
+  changeLongitude,
+  changeLavoura,
+  changeData,
+  changeEvento,
+  clean,
+} = lostCommunicationSlice.actions;
 
 export default lostCommunicationSlice.reducer;
