f7ee73a6cc71fa98eaa0b9e6d1f38dafce712603
Leonidas <leonidas.drawanz@gmail.com>
2022-09-06 22:45:14 -0300

    feat: criando a UpdatePage

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import SearchPage from './pages/searchPage/SearchPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './pages/homePage/HomePage';
 import store from './redux/store';
+import UpdatePage from './pages/updatePage/UpdatePage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
@@ -15,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<HomePage />} />
         <Route path="/insert" element={<InsertPage />} />
         <Route path="/search" element={<SearchPage />} />
-        <Route path="/delete" element={<InsertPage />} />
+        <Route path="/update" element={<UpdatePage />} />
       </Routes>
     </BrowserRouter>
   </Provider>
---
+++ src/pages/updatePage/UpdatePage.jsx
@@ -0,0 +1,87 @@
+import { Button, Spinner } from 'react-bootstrap';
+import Axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import insertValidations from '../../helpers/insertValidations.js';
+import GeneralForm from '../../components/generalForm/GeneralForm.jsx';
+import { clearStore } from '../../redux/reducers/lostCommunicationSlice.js';
+
+export default function UpdatePage() {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const {
+    nome,
+    email,
+    CPF,
+    latitude,
+    longitude,
+    lavoura,
+    data_colheita,
+    evento,
+  } = useSelector((state) => state.lost);
+
+  const payload = {
+    nome,
+    email,
+    CPF,
+    latitude,
+    longitude,
+    lavoura,
+    data_colheita,
+    evento,
+  };
+
+  const request = async (dataJson) => {
+    try {
+      setLoading(true);
+      const { data } = await Axios({
+        method: 'put',
+        url: `https://softfocus-lc-api.herokuapp.com/lost/${CPF}`,
+        headers: { 'Content-Type': 'application/json' },
+        data: dataJson,
+      });
+      alert(data.message);
+      setLoading(false);
+      dispatch(clearStore());
+      navigate('/')
+    } catch (e) {
+      alert(e.response.data.detail);
+      setLoading(false);
+    }
+  };
+
+  const handleUpdate = async () => {
+    const verifyingInsert = insertValidations(payload);
+    if (verifyingInsert.message) {
+      alert(verifyingInsert.message);
+    }
+    if (verifyingInsert.message === undefined) {
+      const dataJson = JSON.stringify(payload);
+      await request(dataJson);
+    }
+  };
+
+  return (
+    <Container>
+      <GeneralForm />
+      {loading ? (
+        <Button variant="primary" disabled>
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          />
+          Loading...
+        </Button>
+      ) : (
+        <Button onClick={() => handleUpdate()}>Atualizar</Button>
+      )}
+    </Container>
+  );
+}
