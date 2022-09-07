cd03431a30e9bebe7c5f1b0a0bccfa83b6c552eb
Leonidas <leonidas.drawanz@gmail.com>
2022-09-06 22:43:15 -0300

    fix: deixando a aplicação apenas com 1 estado

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import newLostCommunicationReducer from './reducers/newLostCommunicationSlice';
 import lostCommunicationReducer from './reducers/lostCommunicationSlice'
 
 export default configureStore({
   reducer: {
-    newLost: newLostCommunicationReducer,
     lost: lostCommunicationReducer,
   },
 });
