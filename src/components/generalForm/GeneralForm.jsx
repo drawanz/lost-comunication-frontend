2f340d7fd83fed1c4f891673f0827a0437136e40
Leonidas <leonidas.drawanz@gmail.com>
2022-09-07 15:37:12 -0300

    feat: adicionando css no generalForm

--- src/components/generalForm/GeneralForm.jsx
+++ src/components/generalForm/GeneralForm.jsx
@@ -1,3 +1,4 @@
+import { Container } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -10,6 +11,7 @@ import {
   changeLongitude,
   changeName,
 } from '../../redux/reducers/lostCommunicationSlice';
+import './style.scss';
 
 export default function GeneralForm() {
   const dispatch = useDispatch();
@@ -26,84 +28,87 @@ export default function GeneralForm() {
   } = useSelector((state) => state.lost);
 
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="nameFormGroup">
-        <Form.Label>Nome do produtor rural</Form.Label>
-        <Form.Control
-          name="nome"
-          type="text"
-          onChange={(e) => dispatch(changeName(e.target.value))}
-          value={nome}
-          placeholder="Ex: José Guilherme Oliveira"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="emailFormGroup">
-        <Form.Label>Email do produtor rural</Form.Label>
-        <Form.Control
-          type="email"
-          onChange={(e) => dispatch(changeEmail(e.target.value))}
-          value={email}
-          placeholder="Ex: jose.guilherme@exemplo.com"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="cpfFormGroup">
-        <Form.Label>CPF do produtor rural</Form.Label>
-        <Form.Control
-          type="text"
-          onChange={(e) => dispatch(changeCpf(e.target.value))}
-          value={CPF}
-          placeholder="Ex: 124.926.160-02 ou 12492616002"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="localizaçãoFormGroup">
-        <Form.Label>Localização da lavoura</Form.Label>
-        <Form.Control
-          type="number"
-          onChange={(e) => dispatch(changeLatitude(e.target.value))}
-          value={latitude}
-          placeholder="Latitude"
-        />
-        <Form.Control
-          type="number"
-          onChange={(e) => dispatch(changeLongitude(e.target.value))}
-          value={longitude}
-          placeholder="Longitude"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="typeCropFormGroup">
-        <Form.Label>Tipo de lavoura</Form.Label>
-        <Form.Control
-          type="text"
-          onChange={(e) => dispatch(changeLavoura(e.target.value))}
-          value={lavoura}
-          placeholder="Ex: Milho"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="dataFormGroup">
-        <Form.Label>Data da colheita</Form.Label>
-        <Form.Control
-          type="text"
-          onChange={(e) => dispatch(changeData(e.target.value))}
-          value={data_colheita}
-          placeholder="Ex: 19/09/1994"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="dateFormGroup">
-        <Form.Label>Evento ocorrido</Form.Label>
-        <Form.Select
-          aria-label="Selecione evento"
-          onChange={(e) => dispatch(changeEvento(e.target.value))}
-          value={evento}
-        >
-          <option>Selecione um evento</option>
-          <option value="Chuva excessiva">Chuva excessiva</option>
-          <option value="Geada">Geada</option>
-          <option value="Granizo">Granizo</option>
-          <option value="Seca">Seca</option>
-          <option value="Vendaval">Vendaval</option>
-          <option value="Raio">Raio</option>
-        </Form.Select>
-      </Form.Group>
-    </Form>
+    <Container className="general-form">
+      <Form>
+        <Form.Group className="mb-3" controlId="nameFormGroup">
+          <Form.Label>Nome do produtor rural</Form.Label>
+          <Form.Control
+            name="nome"
+            type="text"
+            onChange={(e) => dispatch(changeName(e.target.value))}
+            value={nome}
+            placeholder="Ex: José Guilherme Oliveira"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="emailFormGroup">
+          <Form.Label>Email do produtor rural</Form.Label>
+          <Form.Control
+            type="email"
+            onChange={(e) => dispatch(changeEmail(e.target.value))}
+            value={email}
+            placeholder="Ex: jose.guilherme@exemplo.com"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="cpfFormGroup">
+          <Form.Label>CPF do produtor rural</Form.Label>
+          <Form.Control
+            type="text"
+            onChange={(e) => dispatch(changeCpf(e.target.value))}
+            value={CPF}
+            placeholder="Ex: 124.926.160-02 ou 12492616002"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="localizaçãoFormGroup">
+          <Form.Label>Localização da lavoura</Form.Label>
+          <Form.Control
+            type="number"
+            onChange={(e) => dispatch(changeLatitude(e.target.value))}
+            value={latitude}
+            placeholder="Latitude"
+          />
+          <Form.Control
+            className="longitude-field"
+            type="number"
+            onChange={(e) => dispatch(changeLongitude(e.target.value))}
+            value={longitude}
+            placeholder="Longitude"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="typeCropFormGroup">
+          <Form.Label>Tipo de lavoura</Form.Label>
+          <Form.Control
+            type="text"
+            onChange={(e) => dispatch(changeLavoura(e.target.value))}
+            value={lavoura}
+            placeholder="Ex: Milho"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="dataFormGroup">
+          <Form.Label>Data da colheita</Form.Label>
+          <Form.Control
+            type="text"
+            onChange={(e) => dispatch(changeData(e.target.value))}
+            value={data_colheita}
+            placeholder="Ex: 19/09/1994"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="dateFormGroup">
+          <Form.Label>Evento ocorrido</Form.Label>
+          <Form.Select
+            aria-label="Selecione evento"
+            onChange={(e) => dispatch(changeEvento(e.target.value))}
+            value={evento}
+          >
+            <option>Selecione um evento</option>
+            <option value="Chuva excessiva">Chuva excessiva</option>
+            <option value="Geada">Geada</option>
+            <option value="Granizo">Granizo</option>
+            <option value="Seca">Seca</option>
+            <option value="Vendaval">Vendaval</option>
+            <option value="Raio">Raio</option>
+          </Form.Select>
+        </Form.Group>
+      </Form>
+    </Container>
   );
 }
